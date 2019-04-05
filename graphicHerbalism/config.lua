2c71eede524bb6f83d33a26d46ca99bff7472db5
Greatness7 <Greatness7@gmail.com>
2019-04-05 13:14:56 -0400

    Initial commit.

---
+++ graphicHerbalism/config.lua
@@ -0,0 +1,9 @@
+return mwse.loadConfig("graphicHerbalism") or {
+	showTooltips = true,
+	blocked = {
+		["barrel_01_ahnassi_drink"] = true,
+		["barrel_01_ahnassi_food"] = true,
+		["com_chest_02_mg_supply"] = true,
+		["com_chest_02_fg_supply"] = true
+	},
+}
