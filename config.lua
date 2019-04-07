338c4ab5747e54f879d31028d89a977adcb8cb80
Greatness7 <Greatness7@gmail.com>
2019-04-06 21:20:53 -0400

    Spaces > Tabs

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 return mwse.loadConfig("graphicHerbalism") or {
     volume = 50,
-	showTooltips = true,
-	blacklist = {
-		["barrel_01_ahnassi_drink"] = true,
-		["barrel_01_ahnassi_food"] = true,
-		["com_chest_02_mg_supply"] = true,
-		["com_chest_02_fg_supply"] = true
-	},
+    showTooltips = true,
+    blacklist = {
+        ["barrel_01_ahnassi_drink"] = true,
+        ["barrel_01_ahnassi_food"] = true,
+        ["com_chest_02_mg_supply"] = true,
+        ["com_chest_02_fg_supply"] = true
+    },
     whitelist = {}
 }
